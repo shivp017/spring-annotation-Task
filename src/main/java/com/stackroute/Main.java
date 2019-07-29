8295516a236cb00dad6e8ae7ea3e9319f7ba0c80
shiv.prakash <shiv.prakash@cgi.com>
2019-07-29 15:36:56 +0530

    Task 2 completed

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -12,8 +12,15 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Appconfig.class);
-        Movie movie = context.getBean("movie", Movie.class);
+        Movie movie= context.getBean("movie", Movie.class);
         System.out.println(movie);
+
+
+
+        Actor actor1=context.getBean("actor",Actor.class);
+        Actor actor=context.getBean("actor",Actor.class);
+        System.out.println(actor1);
+        System.out.println(actor==actor1);
 
     }
 
--- src/main/java/com/stackroute/domain/Appconfig.java
+++ src/main/java/com/stackroute/domain/Appconfig.java
@@ -2,21 +2,30 @@ package com.stackroute.domain;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class Appconfig {
 
-@Bean
+    @Bean
+    @Scope("prototype")
     public Actor actor() {
-    Actor actor = new Actor();
-    return actor;
-}
+        Actor actor = new Actor();
+        return actor;
+    }
+
 
 @Bean
     public Movie movie() {
     Movie movie = new Movie();
+    movie.setActor(actor());
     return movie;
-
 }
+    @Bean
+    public Movie movie2 ()
+    {
+        Movie movie= new Movie();
+        return movie;
 
+    }
 }
