3b51652ebb8e8174d80d316dd862faf0e3be7d5c
shiv.prakash <shiv.prakash@cgi.com>
2019-07-29 16:01:18 +0530

    Task 4 commit

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,16 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie  implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 @Autowired
     private Actor actor;
 
@@ -31,5 +37,22 @@ public class Movie {
         return "Movie{" +
                 "actor=" + actor +
                 '}';
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("bean factory ");
+
+    }
+
+    @Override
+    public void setBeanName(String s) throws BeansException{
+        System.out.println("bean name");
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application context");
     }
 }
